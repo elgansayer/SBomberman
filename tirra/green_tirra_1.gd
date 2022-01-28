7e89cdefedf6d8083f82ddcc591e39fde373f976
Elgan <elgan.sayer@interfolio.com>
2022-01-28 17:36:45 +0000

    Add skidding!!

---
+++ tirra/green_tirra_1.gd
@@ -0,0 +1,118 @@
+extends "res://tirra/green_tirra.gd"
+
+var last_direction
+var direction_table = {"up": "down", "down": "up", "left": "right", "right": "left"}
+
+
+func update_animation(animation_name):
+	if animation_name == $AnimatedSprite.animation:
+		return
+
+	if state == states.STATE_IDLE:
+		$AnimatedSprite.play(animation_name)
+
+
+func action(player):
+	last_direction = rider.current_animation_direction
+	.action(player)
+
+
+func should_skid():
+	return rider.pressed_direction == direction_table[rider.current_animation_direction]
+
+
+func skid():
+	print("skid")
+	# rider.frozen_animation = true
+	# rider.frozen_movement = true
+	state = states.STATE_SKIDDING
+	$AnimatedSprite/RightFeetAnimatedSprite.visible = false
+	$AnimatedSprite/LeftFeetAnimatedSprite.visible = false
+
+	var animation = "skid_" + rider.current_animation_direction
+	$AnimationPlayer.play(animation)
+
+
+func check_rider_stopped():
+	.check_rider_stopped()
+
+	print(rider.pressed_direction, " ", direction_table[rider.current_animation_direction])
+
+	if should_skid():
+		print("should_skid() ", should_skid())
+		skid()
+
+
+func reset_rider_speed():
+	rider.extra_speed = 200
+
+
+func hit_wall():
+	.hit_wall()
+	$AnimatedSprite.play("standing_" + rider.current_animation_direction)
+
+	$AnimatedSprite/RightFeetAnimatedSprite.visible = false
+	$AnimatedSprite/LeftFeetAnimatedSprite.visible = false
+
+# extends "res://tirra/green_tirra.gd"
+
+# var last_direction
+
+# func action(player):
+# 	player.forced_move = true
+# 	player.extra_speed = 200
+# 	state = states.STATE_ACTION
+# 	update_speed_anim()
+# 	$Timer.start()
+
+# func update_speed_anim():
+# 	last_direction = rider.current_animation_direction
+# 	var animation = "action_" + last_direction
+# 	$AnimationPlayer.play(animation)
+
+# func update_animation(animation_name):
+# 	if animation_name == $AnimatedSprite.animation:
+# 		return
+
+# 	if state == states.STATE_IDLE:
+# 		$AnimatedSprite.play(animation_name)
+# 	elif state == states.STATE_STOPPED:
+# 		$AnimatedSprite.play("standing_" + rider.current_animation_direction)
+# 	elif last_direction != rider.current_animation_direction:
+# 		update_speed_anim()
+
+# func _on_Timer_timeout():
+# 	rider.forced_move = false
+# 	rider.extra_speed = 0
+# 	state = states.STATE_IDLE
+# 	$AnimatedSprite/RightFeetAnimatedSprite.visible = false
+# 	$AnimatedSprite/LeftFeetAnimatedSprite.visible = false
+
+# func same_spot():
+# 	if state == states.STATE_STOPPED:
+# 		return
+
+# 	print("same_spot")
+# 	var collision = rider.get_last_slide_collision()
+# 	if collision && collision.collider:
+# 		state = states.STATE_STOPPED
+# 		$AnimatedSprite/RightFeetAnimatedSprite.visible = false
+# 		$AnimatedSprite/LeftFeetAnimatedSprite.visible = false
+
+# func _physics_process(_delta):
+# 	if !rider:
+# 		return
+
+# 	if state == states.STATE_IDLE:
+# 		return
+
+# 	# We are in the same place
+# 	if last_position == rider.position || rider.motion.x == 0 && rider.motion.y == 0:
+# 		same_spot()
+# 	elif state == states.STATE_STOPPED:
+# 		update_speed_anim()
+# 	else:
+# 		state = states.STATE_ACTION
+
+# 	# Set the last position
+# 	last_position = rider.position
