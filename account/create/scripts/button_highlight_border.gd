93ef0181e3be1658bf84f94727c59e0e3c6ebd02
Elgan <elgan.sayer@interfolio.com>
2022-03-15 20:54:35 +0000

    try and make border dynamic

--- account/create/scripts/button_highlight_border.gd
+++ account/create/scripts/button_highlight_border.gd
@@ -1,6 +1,29 @@
 extends Node2D
 
+@export var size_offset: Vector2 = Vector2(4, 3)
+@export var sizable: NodePath
+@export var show_indicators: bool = true
+@export var indicators: Array[NodePath] = []
+
+var ninePatchBorder: NinePatchRect
+
+func _ready():
+	var size = get_parent().get_size()
+	ninePatchBorder = get_node(sizable)
+	var newSize = size + size_offset
+	ninePatchBorder.set_size(newSize)
+			
+	for indicatorPath in indicators:
+		var indicator = get_node(indicatorPath)
+		indicator.visible = show_indicators
+			
+			
+	var indicator2 = get_node(indicators[0])
+	var indicator1 = get_node(indicators[1])
+		
+	var newPos = newSize / 2
+	indicator1.position = Vector2(newPos.x, 0)
+	indicator2.position = Vector2(newPos.x, newSize.y)
+	
 func _on_timer_timeout():
-	var node = get_node("Node2D")
-	var patch = node.get_node("NinePatchBorder")
-	patch.visible = !patch.visible
+	ninePatchBorder.visible = !ninePatchBorder.visible
